de04c1863cbe5f303b2139e4e3b1c07bda4e7497
yoshiyama <yamamoto.ait@gmail.com>
2023-06-15 19:48:59 +0900

    first commit

--- labelme_json_to_png_L.py
+++ labelme_json_to_png_L.py
@@ -1,5 +1,6 @@
 # python labelme_json_to_png.py <LabelmeのJSONファイルが入っているフォルダ.もとのjpgが入っていてもいい> -o=<出力先フォルダ> -label_file=<ラベルファイルのパス>
 # python labelme_json_to_png.py kuroda -o=kuroda_output -label_file=kuroda_txt
+# python labelme_json_to_png_L.py /mnt/c/Users/survey/Desktop/keikan_bridge/CycleGAN_Mask/ROOT41/kiritori-gero1 -o=/mnt/c/Users/survey/Desktop/keikan_bridge/CycleGAN_Mask/ROOT41/kiritori-gero1_mask -class_name=hodoukyou -pixel_value=255
 
 #
 # ラベルファイルとは、セグメンテーション対象を記載したテキストファイル。例えば、kimura_txtとかを参照してください。
@@ -10,80 +11,67 @@ import shutil
 import argparse
 import tqdm
 from PIL import Image
-from PIL import ImageChops
 import numpy as np
+import matplotlib.pyplot as plt
 
 TEMP_DIR = "temp"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("json_dir", help="JSON files directory.")
 parser.add_argument("-o", required=True, help="Output directory.")
-parser.add_argument("-label_file", required=True, help="Label name file.")
+parser.add_argument("-class_name", required=True, help="Class name to be masked.")
+parser.add_argument("-pixel_value", required=True, type=int, help="Pixel value to fill the mask.")
 
 args = parser.parse_args()
 
 json_dir_path = Path(args.json_dir)
 out_dir_path = Path(args.o)
-label_file_path = Path(args.label_file)
+class_name = args.class_name
+pixel_value = args.pixel_value
 
-assert json_dir_path.exists(), "JSON directory is not exist."
-assert out_dir_path.exists(), "Output directory is not exist."
-assert label_file_path.exists(), "Label file is not exist."
+assert json_dir_path.exists(), "JSON directory does not exist."
+assert out_dir_path.exists(), "Output directory does not exist."
 
-# Preprocessing.
-correct_label_dict = {}
-with open(str(label_file_path), "r") as file:
-    for i, line in enumerate(file):
-        correct_label_dict[line.replace("\n", "")] = i + 1
 temp_dir_path = out_dir_path.joinpath(TEMP_DIR)
-print(temp_dir_path)
 if not temp_dir_path.exists():
     temp_dir_path.mkdir()
 
-# Batch process Converting json to png.
+# Batch process: Convert JSON to PNG.
 for json_file_path in tqdm.tqdm(list(json_dir_path.glob("*.json"))):
-    # Execute "labelme_json_to_dataset".
-    print(json_file_path)
-    print(temp_dir_path)
-    print("go")
-    subprocess.run(["labelme_json_to_dataset", str(json_file_path), "-o", str(temp_dir_path)], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    subprocess.run(["labelme_json_to_dataset", str(json_file_path), "-o", str(temp_dir_path)], stdout=subprocess.PIPE,
+                   stderr=subprocess.PIPE, text=True)
 
-    print("go2")
+    # image = Image.open(str(temp_dir_path.joinpath("label.png"))).convert("L")
     image = Image.open(str(temp_dir_path.joinpath("label.png"))).convert("P")
-    origin_color_palette = image.getpalette()
-    print("origin_color_palette=", origin_color_palette)
-    # print(origin_color_palette)
+    # plt.imshow(image, cmap='gray')
+    # plt.show()
     image_array = np.array(image)
+    # plt.imshow(image_array, cmap='gray')
+    # plt.show()
 
-    # Fix label.
-    label_indexes = {}
+    class_id = None
     with open(str(temp_dir_path.joinpath("label_names.txt")), "r") as label_file:
         for label_num, label_name in enumerate(label_file):
             label_name = label_name.replace("\n", "")
-            if label_name == "_background_":
-                continue
-            if label_name == "line":
-                continue
-            label_indexes[label_name] = (image_array == label_num)
-            print(label_name)
-            print(label_num)
+            if label_name == class_name:
+                class_id = label_num
+                print(f"Class '{class_name}' found as class ID {class_id} in {json_file_path}")
 
-        for label_name, label_index in label_indexes.items():
-            correct_label_num = correct_label_dict[label_name]
-            image_array[label_index] = correct_label_num
-            print("correct_label_num\n")
-            print(correct_label_num)
-    print("first")
-    print(image_array)
-    # new_image = Image.fromarray(image_array, mode="P")
-    new_image = Image.fromarray(image_array.astype(np.uint8), mode="L")
-    print(origin_color_palette)
-    # new_pallete = new_image.putpalette(origin_color_palette)
-    print(new_image)
-    # new_image.save(str(out_dir_path.joinpath(json_file_path.name).with_suffix(".png")))
-    new_image.save(str(out_dir_path.joinpath(json_file_path.name).with_suffix(".png")), format="PNG")
+    if class_id is not None:
+        print("class_id=", class_id)
+        print("pixel_value=", pixel_value)
+        image_array[image_array == class_id] = pixel_value
+        # plt.imshow(image_array, cmap='gray')
+        # plt.show()
+        new_image = Image.fromarray(image_array, mode="L")
+        # plt.imshow(new_image, cmap='gray')
+        # plt.show()
+        new_image.save(str(out_dir_path.joinpath(json_file_path.stem).with_suffix(".png")))
+        print(f"Image saved to {str(out_dir_path.joinpath(json_file_path.stem).with_suffix('.png'))}")
+    else:
+        print(f"Class '{class_name}' not found in {json_file_path}")
 
-# Post processing.
+# Post-processing.
 if temp_dir_path.exists():
     shutil.rmtree(str(temp_dir_path))
 
